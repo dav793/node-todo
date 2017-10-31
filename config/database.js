a1e94e8088b584cb20eb135271651a5580959f13
David Vargas <dav793@gmail.com>
2017-10-31 02:04:28 -0600

    test

--- config/database.js
+++ config/database.js
@@ -1,4 +1,6 @@
 module.exports = {
-    remoteUrl : 'mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu',
+    remoteUrl : 'mongodb://meanstacktutorials:meanstacktutorials@ec2-54-233-147-137.sa-east-1.compute.amazonaws.com:27017/meanstacktutorials',
     localUrl: 'mongodb://localhost/meanstacktutorials'
 };
+
+// mongodb://{NEW USERNAME}:{NEW PASSWORD}@{EC2 URL}:{PORT}/{DATABASE NAME}
